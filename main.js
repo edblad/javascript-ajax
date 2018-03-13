6038d0d7905ae6c1a63fdefdb4ad5443a83c5092
Helena Edblad <helena.edblad@gmail.com>
2018-03-13 10:27:15 +0100

    able to print departure and arrival trains

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 const authKey = '08fd13eb5afe4d5dbc7ba3a4ddd5ddcd';
-const button = document.getElementById('button');
-const searchCity = document.getElementById('searchCity');
 let station = '';
+let stationName = '';
+let departArrive = '';
+let time = '';
+let trainNumber = '';
+let track = '';
+
+const searchCity = document.getElementById('searchCity');
+const output = document.getElementById('output');
+const departure = document.getElementById('departure');
+const arrival = document.getElementById('arrival');
+
+
+// EVENT LISTENERS //
+departure.addEventListener('click', function(){
+    departArrive = 'Avgang';
+})
+
+arrival.addEventListener('click', function(){
+    departArrive = 'Ankomst';
+})
+
+searchCity.addEventListener('change', function(){
+    const searchValue = searchCity.value;
+    fetchStation(searchValue);
+})
 
 // Request to fetch all train stations in Sweden
 const stationsRequest = `
@@ -19,6 +42,8 @@ const stationsOptions = {
     body: stationsRequest
 }
 
+
+// FUNCTIONS //
 function fetchStation(searchValue){
     fetch('http://api.trafikinfo.trafikverket.se/v1.3/data.json', stationsOptions)
         .then(function(response){       
@@ -43,7 +68,7 @@ function fetchTimeTable(){
       <QUERY objecttype="TrainAnnouncement" orderby="AdvertisedTimeAtLocation">
         <FILTER>
           <AND>
-            <EQ name="ActivityType" value="Avgang" />
+            <EQ name="ActivityType" value="${departArrive}" />
             <EQ name="LocationSignature" value="${station}" />
             <OR>
               <AND>
@@ -77,49 +102,12 @@ function fetchTimeTable(){
         })
         .then(function(data){      
             console.log(data);
-            
-            //showStation(data);
+            showTrains(data);
         })
         .catch(function(error){
             console.log(error)
         });
 }
-
-//fetchStation();
-//fetchTimeTable();
-
-//function returnStation(data){
-//    const dataArray = data.RESPONSE.RESULT[0].TrainStation;
-//    console.log(data.RESPONSE.RESULT[0]);
-//    for(i = 0; i < dataArray.length; i++){
-//        if(dataArray[i].LocationSignature == 'A'){
-//            //console.log(dataArray[i].AdvertisedLocationName);
-//        }
-//    }
-//    //console.log(dataArray);
-//}
-//
-//function showSomething(data){
-//    const dataArray = data.RESPONSE.RESULT[0].TrainAnnouncement;
-//    console.log(data.RESPONSE.RESULT[0]);
-//    let clickedLocation = '';
-//    for(i = 0; i < dataArray.length; i++){
-//        clickedLocation = dataArray[200].ToLocation[0].LocationName;
-//    }
-//    //console.log(clickedLocation);
-//    //return clickedLocation;
-//}
-
-
-button.addEventListener('click', function(){
-    fetchTimeTable();
-    fetchStation();
-});
-
-searchCity.addEventListener('change', function(){
-    const searchValue = searchCity.value;
-    fetchStation(searchValue);
-});
 
 function shortStationName(stationData, searchValue){
     const dataArray = stationData.RESPONSE.RESULT[0].TrainStation;
@@ -133,26 +121,56 @@ function shortStationName(stationData, searchValue){
     }
 }
 
-function showStation(data){
+function fullStationName(stationName, track, time, trainNumber){
+    const request = `
+        <REQUEST>
+          <LOGIN authenticationkey="${authKey}" />
+          <QUERY objecttype="TrainStation">
+            <FILTER>
+                <EQ name="LocationSignature" value="${stationName}" />
+            </FILTER>
+          </QUERY>
+        </REQUEST>
+        `;
+
+    const stationOptions = {
+        method: 'POST',
+        headers: { 'content-type': 'text/xml' },
+        body: request
+    }
     
-    
-    
-    
-    
-    
-//    let dataArray = data.RESPONSE.RESULT[0].TrainAnnouncement;
-//    console.log(dataArray);
-//    console.log(shortStationName);
-//    console.log(station);
-//    for(let i = 0; i < dataArray.length; i++){
-//        if(dataArray[i].ToLocation){
-//            if(shortStationName === dataArray[i].ToLocation[0].LocationName){
-//                console.log(dataArray[i].ToLocation[0].LocationName);
-//                station = dataArray[i].ToLocation[0].LocationName;
-//            }
-//        }
-//    }
+    fetch('http://api.trafikinfo.trafikverket.se/v1.3/data.json', stationOptions)
+        .then(function(response){       
+            return response.json();
+        })
+        .then(function(data){
+            let paragraph = document.createElement('p');
+            let stationName = data.RESPONSE.RESULT[0].TrainStation[0].AdvertisedLocationName;
+            
+            paragraph.innerHTML += `${stationName} <br />`;
+            output.appendChild(paragraph);
+            })
+        .catch(function(error){
+            console.log(error)
+        });
 }
+
+function showTrains(data){
+    let dataArray = data.RESPONSE.RESULT[0].TrainAnnouncement;
+    
+    for(let i = 0; i < dataArray.length; i++){
+        if(dataArray[i].ToLocation){
+            stationName = dataArray[i].ToLocation[0].LocationName;
+            track = dataArray[i].TrackAtLocation;
+            time = dataArray[i].AdvertisedTimeAtLocation;
+            trainNumber = dataArray[i].AdvertisedTrainIdent;
+            
+            fullStationName(stationName, track, time, trainNumber);
+            
+        }
+    }
+}
+
 
 
 
